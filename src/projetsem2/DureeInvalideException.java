34e602690d7933e7341aee7c0a6c6ee32fa9751d
lamul <lamul@192.168.100.114>
2021-03-24 11:07:16 +0100

    Merge origin/master
    Dernières modifications (Kilian) : classes exceptions

---
+++ src/projetsem2/DureeInvalideException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetsem2;
+
+/**
+ *
+ * @author lamul
+ */
+public class DureeInvalideException extends GrilleException {
+
+    /**
+     * Creates a new instance of <code>DureeInvalideException</code> without
+     * detail message.
+     */
+    public DureeInvalideException() {
+    }
+
+    /**
+     * Constructs an instance of <code>DureeInvalideException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public DureeInvalideException(String msg) {
+        super(msg);
+    }
+}
