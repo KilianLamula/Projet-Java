ea692e7a492bf414aba20d0242e69a65e21888e9
lamul <lamul@192.168.100.114>
2021-03-24 12:19:55 +0100

    Merge origin/master
    Dernières modifications (Kilian) : Try catch main

--- src/projetsem2/Main.java
+++ src/projetsem2/Main.java
@@ -6,6 +6,8 @@
 package projetsem2;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,28 +21,33 @@ public class Main {
     public static void main(String[] args) {
         
         //Création de la liste d'émissions
-        ArrayList<Emission> lesEmissions = new ArrayList<Emission>();
-        
-        //Création des émissions
-        Emission e1 = new Fiction("Inception",4,0,2010,true,new Realisateur("Nolan","Christopher"));
-        Emission e2 = new Reportage ("La danse des oiseaux",1,4,Theme.ANIMALIER);
-        Emission e3 = new Fiction ("Independence",4,5,1996,true,new Realisateur("Emmerich","Roland"));
-        Emission e4 = new Fiction ("Avatar",4,9,2009,true,new Realisateur("Cameron","James"));   
-        Emission e5 = new Fiction ("Alita",4,13,2019,true,new Realisateur("Rodriguez","Robert"));
-        Emission e6 = new Fiction ("Passengers",2,17,2016,true,new Realisateur("Tyldum","Morten"));
-        Emission e7 = new Divertissement ("TPMP",2,19,new Animateur("Hamouna","Cyril"));
-        Emission e8 = new Fiction ("Interstellar",3,21,2014,false,new Realisateur("Nolan","Christopher"));
-                
-        //Replissage de la liste
-        lesEmissions.add(e1);
-        lesEmissions.add(e2);
-        lesEmissions.add(e3);
-        lesEmissions.add(e4);
-        lesEmissions.add(e5);
-        lesEmissions.add(e6);
-        lesEmissions.add(e7);
-        lesEmissions.add(e8);
-        
+            ArrayList<Emission> lesEmissions = new ArrayList<Emission>();
+            
+        try {
+            //Création des émissions
+            Emission e1 = new Fiction("Inception",4,0,2010,true,new Realisateur("Nolan","Christopher"));
+            Emission e2 = new Reportage ("La danse des oiseaux",1,4,Theme.ANIMALIER);
+            Emission e3 = new Fiction ("Independence",4,5,1996,true,new Realisateur("Emmerich","Roland"));
+            Emission e4 = new Fiction ("Avatar",4,9,2009,true,new Realisateur("Cameron","James"));
+            Emission e5 = new Fiction ("Alita",4,13,2019,true,new Realisateur("Rodriguez","Robert"));
+            Emission e6 = new Fiction ("Passengers",2,17,2016,true,new Realisateur("Tyldum","Morten"));
+            Emission e7 = new Divertissement ("TPMP",2,19,new Animateur("Hamouna","Cyril"));
+            Emission e8 = new Fiction ("Interstellar",3,21,2014,false,new Realisateur("Nolan","Christopher"));
+            
+            //Replissage de la liste
+            lesEmissions.add(e1);
+            lesEmissions.add(e2);
+            lesEmissions.add(e3);
+            lesEmissions.add(e4);
+            lesEmissions.add(e5);
+            lesEmissions.add(e6);
+            lesEmissions.add(e7);
+            lesEmissions.add(e8);
+            
+        } catch (GrilleException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //Affichage du programme
         System.out.println("Les émissions diffusé dans la journée sont : " + '\n' + lesEmissions);
     }
     
